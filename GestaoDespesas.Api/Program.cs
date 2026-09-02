e2062ab3cf99621960eb81257de73ef8e6e9d399
Matheus3065 <matheus.chaim2000@gmail.com>
2026-09-02 23:38:14 +0100

    Configure Swagger UI with Swashbuckle

--- GestaoDespesas.Api/Program.cs
+++ GestaoDespesas.Api/Program.cs
@@ -3,15 +3,26 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+
+//Register Swagger generator, wacth builds the OpenAPI spec and powers the Swagger UI
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+
+    //enable middleware to serve the generated Swagger JSON document
+    app.UseSwagger();
+
+    //enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "GestaoDespesas API v1");
+    });
 }
 
 app.UseHttpsRedirection();
